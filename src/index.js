42813117c5345f58a1e6af547139ecc1358b94e8
Taha shabaan <tahashabaan48@gmail.com>
2024-03-26 11:45:00 +0200

    empty project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import CartProvider from './store/CartProvider';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <CartProvider> <App /> </CartProvider>);
